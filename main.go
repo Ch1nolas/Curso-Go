e3bff3109acb6fe86e7975c5ef025b2ab4282762
Ch1nolas <nmartinez252524342@gmail.com>
2023-09-24 20:40:07 -0300

    Utilizacion de condicionales por tipoo de so (if else y switch)

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
-	"Github/Ch1nolas/Curso-Go/variables"
 	"fmt"
+	"runtime"
 )
 
 func main() {
-	estado, texto := variables.ConviertoaTexto(1588)
-	fmt.Println(estado)
-	fmt.Println(texto)
+	/*
+		estado, texto := variables.ConviertoaTexto(1588)
+		fmt.Println(estado)
+		fmt.Println(texto)
+	*/
+
+	if os := runtime.GOOS; os == "linux" || os == "OS X." {
+		fmt.Println("Esto no es windows, es ", os)
+	} else {
+		fmt.Println("Esto es windows, es")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es Linux")
+	case "darwin":
+		fmt.Println("Esto es Darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}
+
 }
